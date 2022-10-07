8b10bb20a7e590b20d2aa79011333dc8e5863a38
RSebetoa <RSebetoa@csir.co.za>
2022-10-07 13:17:15 +0200

    Updated Player.cs to include the rendering of different colors for each movement direction

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,10 +6,14 @@ public class Player : MonoBehaviour
 {
     private float speed = 5.0f;
     private Rigidbody rb;
+    new Renderer renderer;
+
     // Start is called before the first frame update
     void Start()
     {
         rb = this.GetComponent<Rigidbody>();
+
+        renderer = this.GetComponent<Renderer>();
     }
 
     // Update is called once per frame
@@ -19,24 +23,28 @@ public class Player : MonoBehaviour
         if (Input.GetKey(KeyCode.W)) 
         {
             rb.MovePosition(transform.position + Vector3.up * speed * Time.deltaTime);
+            renderer.material.SetColor("_Color", Color.red);
         }
 
         //Move Left
         else if (Input.GetKey(KeyCode.A))
         {
             rb.MovePosition(transform.position + Vector3.left * speed * Time.deltaTime);
+            renderer.material.SetColor("_Color", Color.blue);
         }
 
         //Move Right
         else if (Input.GetKey(KeyCode.D))
         {
             rb.MovePosition(transform.position + Vector3.right * speed * Time.deltaTime);
+            renderer.material.SetColor("_Color", Color.black);
         }
 
-        //Move Left
+        //Move Down
         else if (Input.GetKey(KeyCode.S))
         {
             rb.MovePosition(transform.position + Vector3.down * speed * Time.deltaTime);
+            renderer.material.SetColor("_Color", Color.yellow);
         }
     }
 }

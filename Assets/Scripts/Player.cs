586fad1a9d1afee1f4f24bda5f526efa1d13067d
RSebetoa <RSebetoa@csir.co.za>
2022-10-07 15:56:41 +0200

    Added the functionality to handle the re appearance of the object on the opposite side once it goes of the Camera's view

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,19 +8,43 @@ public class Player : MonoBehaviour
     private Rigidbody rb;
     new Renderer renderer;
 
+    private float leftBound = 0.0f;
+    private float rightBound = 0.0f;
+    private float topBound = 0.0f;
+    private float bottomBound = 0.0f;
+
+    private float cubeWidth = 0.0f;
+    private float cubeHeight = 0.0f;
+
     // Start is called before the first frame update
     void Start()
     {
-        rb = this.GetComponent<Rigidbody>();
+        rb = GetComponent<Rigidbody>();
 
-        renderer = this.GetComponent<Renderer>();
+        renderer = GetComponent<Renderer>();
+
+        leftBound = Camera.main.ScreenToWorldPoint( new Vector3(0.0f, 0.0f, 0 - Camera.main.transform.position.z) ).x;
+        rightBound = Camera.main.ScreenToWorldPoint( new Vector3(Screen.width, 0.0f, 0 - Camera.main.transform.position.z) ).x;
+        bottomBound = Camera.main.ScreenToWorldPoint(new Vector3(0.0f, 0.0f, 0 - Camera.main.transform.position.z)).y;
+        topBound = Camera.main.ScreenToWorldPoint(new Vector3(0.0f, Screen.height, 0 - Camera.main.transform.position.z)).y;
+
+        cubeWidth = transform.localScale.x;
+        cubeHeight = transform.localScale.y;
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        MoveAndChangeColor();
+
+        KeepObjectInView();
+    }
+
+    public void MoveAndChangeColor()
+    {
         //Move UP
-        if (Input.GetKey(KeyCode.W)) 
+        if (Input.GetKey(KeyCode.W))
         {
             rb.MovePosition(transform.position + Vector3.up * speed * Time.deltaTime);
             renderer.material.SetColor("_Color", Color.red);
@@ -46,5 +70,33 @@ public class Player : MonoBehaviour
             rb.MovePosition(transform.position + Vector3.down * speed * Time.deltaTime);
             renderer.material.SetColor("_Color", Color.yellow);
         }
+    }
+
+    public void KeepObjectInView()
+    {
+        //Out of Field of view on the left
+        if (transform.position.x < leftBound - cubeWidth/2)
+        {
+            transform.position = new Vector3(rightBound - cubeWidth / 2, transform.position.y, transform.position.z);  
+        }
+
+        //Out of Field of view on the right
+        else if (transform.position.x > rightBound + cubeWidth/2)
+        {
+            transform.position = new Vector3(leftBound + cubeWidth / 2, transform.position.y, transform.position.z);
+        }
+
+        //Out of Field of view on the top
+        else if (transform.position.y > topBound + cubeHeight/2)
+        {
+            transform.position = new Vector3(transform.position.x, bottomBound + cubeHeight / 2, transform.position.z);
+        }
+
+        //Out of Field of view on the bottom
+        else if (transform.position.y < bottomBound - cubeHeight / 2)
+        {
+            transform.position = new Vector3(transform.position.x, topBound - cubeHeight / 2, transform.position.z);
+        }
+
     }
 }
